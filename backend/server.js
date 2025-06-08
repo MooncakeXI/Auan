e77ca7b0a66e191ccadca06d2b4c8d910e877884
Suvijak <Suvijak237@gmail.com>
2025-06-08 23:07:32 +0700

    fix:add vercel.json again

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const serverless = require('serverless-http');
 
 
 dotenv.config({ path: './config/config.env' });
@@ -17,21 +18,4 @@ app.use(express.json());
 app.use('/api/wishes', wishRoutes);
 app.use('/api/memories', memoryRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-const server = app.listen(
-  PORT,
-  console.log(
-    "Server running in ",
-    process.env.NODE_ENV,
-    " mode on port ",
-    PORT
-  )
-);
-
-process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
-  server.close(() => process.exit(1));
-});
-
-module.exports = app;
+module.exports.handler = serverless(app);
